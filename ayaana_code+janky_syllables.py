c700c7b092efb6ed1ad272e93a026d3639d88139
Gallinaryoso <henryandrewbaer@gmail.com>
2017-02-23 15:49:39 -0800

    Add files via upload

--- ayaana_code+janky_syllables.py
+++ ayaana_code+janky_syllables.py
@@ -21,6 +21,24 @@ lines = lines[ notblank ]
 notnumber = np.where( [ line[:5] != '     ' for line in lines ] )[0]
 lines = lines[ notnumber ]
 
+def nsyl(word):
+	word = word.lower()
+	if word in {"churl","feed'st", "gild'st", "gav'st", "know'st", "sway'st", "mak'st", "dearths", "'gainst", "'this", "'truth", "stick'st", "'had"}:
+		return 1
+	if word in {"thriftless", "glutton", "awakes", "bosoms","didst", "thrivers", "filching", "dateless", "savour", "wherefore","wand'rest", "compeers", "shouldst", "blenches", "basest", "o'ersways", "riper", "burthen", "humour", "diest", "freezings"}:
+		return 2
+	if word in {"all-eating", "niggarding", "self-substantial", "equipage", "usurer", "honouring", "warrantise", "amazeth", "buriest", "sepulchres", "departest", "convertest", "cherubins", "shallowest"}:
+		return 3
+	if word in {"incertainty", "inheritors"}:
+		return 4
+	if word in {"'","'s"}:
+		return 11
+	if word not in d:
+		return len(''.join(" x"[c in"aeiouyAEIOUY"]for c in(word[:-1]if'e'==word[-1]else word)).split())
+
+	return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]][0]
+
+
 for l in range(len(lines)):
 	lines[l] = lines[l].replace('.','')
 	lines[l] = lines[l].replace(',','')
@@ -74,20 +92,6 @@ tag_map = { type_map[key]: key for key in type_map.keys() }
 def nsyl(word):
 	return len(''.join(c if c in"aeiouyAEIOUY"else' 'for c in word.rstrip('e')).split())
 '''
-
-def nsyl(word):
-	word = word.lower()
-	if word in {"sway'st", "mak'st", "dearths", "'gainst", "'this", "'truth", "stick'st", "'had"}:
-		return 1
-	if word in {"thrivers", "filching", "dateless", "savour", "wherefore","wand'rest", "compeers", "shouldst", "blenches", "basest", "o'ersways", "riper", "burthen", "humour", "diest", "freezings"}:
-		return 2
-	if word in {"equipage", "usurer", "honouring", "warrantise", "amazeth", "buriest", "sepulchres", "departest", "convertest", "cherubins", "shallowest"}:
-		return 3
-	if word in {"incertainty", "inheritors"}:
-		return 4
-	if word in {"'","'s"}:
-		return 11
-	return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]][0]
 
 poem = [[] for i in range(14)]
 
