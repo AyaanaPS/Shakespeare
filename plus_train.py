7513a774d9e79770e510615eb39eafea23f93b4f
Henry Baer <henrybaer@Henrys-MacBook-Pro.local>
2017-02-25 11:18:17 -0800

    adding pkls

--- plus_train.py
+++ plus_train.py
@@ -8,14 +8,9 @@ X = pickle.load(open('data%d.pkl' % data_ver,'rb'))
 
 from HMM import unsupervised_HMM
 
-n_states = 50
+n_states = 20
 n_iters = 2000
 
-for n_states in [4]:
-
-	for n_iters in [1000]:
-
-		HMM = unsupervised_HMM(X, n_states, n_iters)
-
-		pickle.dump(HMM,open('HMM_%d_%d_%d.pkl' % (data_ver,n_states,n_iters),'wb'))
+HMM = unsupervised_HMM(X, n_states, n_iters)
+pickle.dump(HMM,open('HMM_%d_%d_%d.pkl' % (data_ver,n_states,n_iters),'wb'))
 
